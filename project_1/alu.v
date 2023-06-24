a6bb28b4b5aaa3dd30dd78e2bf35ff475a3effb2
Invariant64 <98544022+Invariant64@users.noreply.github.com>
2023-06-24 20:38:45 +0800

    add constants

--- project_1/alu.v
+++ project_1/alu.v
@@ -1,3 +1,5 @@
+`include "defines.v"
+
 module alu (
     input[1:0] alu_ctl,
     input [31:0] A, B,
@@ -5,14 +7,13 @@ module alu (
     output [31:0] C
 );
 
-    // alu_ctl : 2'b00 = addu, 2'b01 = subu, 2'b10 = ori, 2'b11 = lui
-    assign C = (alu_ctl == 2'b00) ? A + B :
-               (alu_ctl == 2'b01) ? A - B :
-               (alu_ctl == 2'b10) ? A | B :
-               (alu_ctl == 2'b11) ? {B[15:0], 16'b0} : 32'b0;
+    assign C = (alu_ctl == `ALU_OP_ADD) ? A + B :
+               (alu_ctl == `ALU_OP_SUB) ? A - B :
+               (alu_ctl == `ALU_OP_OR) ? A | B :
+               (alu_ctl == `ALU_OP_LUI) ? {B[15:0], 16'b0} : 32'b0;
 
     assign zero = (C == 32'b0);
     assign positive = (C[31] == 1'b0) && !zero;
-    assign overflow = (alu_ctl == 2'b00) ? (A[31] == B[31] && C[31] != A[31]) : 1'b0;     
+    assign overflow = (alu_ctl == `ALU_OP_ADD) ? (A[31] == B[31] && C[31] != A[31]) : 1'b0;     
 
 endmodule
